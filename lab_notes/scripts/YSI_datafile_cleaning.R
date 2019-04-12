985e0a4a676d132f0e833312bf42deebbd9f42fc
Kenneth Fortino <fortinok@lomgwood.edu>
2019-04-12 15:47:46 -0400

    added metadata to YSI script

--- lab_notes/scripts/YSI_datafile_cleaning.R
+++ lab_notes/scripts/YSI_datafile_cleaning.R
@@ -1,17 +1,30 @@
 # Cleaning Script for .csv files that are exported from the YSI EXO2
 
-## Import the data file as text
+## Input filenames
 
    input.raw.file <- "./data/chalgrove_lake_4April2019_raw.csv"
+   
+   output.raw.data.file <- "./data/chalgrove_lake_4April2019_raw_data.csv" # file name format = ./data/lake_date_raw_data.csv
+   
+   output.clean.file <- "./data/chalgrove_lake_4April2019.csv" 
+    # file name format = ./data/lake_date.csv
+   
+   metadata.file <- "./metadata/chalgrove_lake_4April2019_metadata.txt" 
+    # file name format = ./data/lake_date.csv
+   
+  
+## Import the data file as text
+   
    YSI.raw <- readLines(input.raw.file, skipNul = T)
 
 ### Notes
 
 At this point the data file contains 18 lines of metadata before the data begin.
 
-## Remove metadata
+## Extract metadata
 
-    YSI.raw.rows<- YSI.raw[-c(1:18) ]
+    YSI.metadata <- YSI.raw[c(1:19)]
+    YSI.raw.rows <- YSI.raw[-c(1:18) ]
 
 ### Notes
     
@@ -23,12 +36,10 @@ Now the file has an extra row between each row of data values
 
 ## Rename the header names in the first row
     
-    YSI.raw.data[1] <- c("date, time, fract_sec, site_name, cond, nLF cond, perc_ODO, perc_local_ODO, ODO_conc, sal, sp_cond, TDS, pH, pH_mV, temp, battery, cable_pwr")
+    YSI.raw.data[1] <- c("date, time, fract_sec, site_name, cond, nLF_cond, perc_ODO, perc_local_ODO, ODO_conc, sal, sp_cond, TDS, pH, pH_mV, temp, battery, cable_pwr")
     
 ## Write to csv file
 
-    output.raw.data.file <- "./data/chalgrove_lake_4April2019_raw_data.csv" # file name format = ./data/lake_date_raw_data.csv
-    
     writeLines(YSI.raw.data, con = output.raw.data.file) 
 
 ## Import raw data-only csv file as a data.frame
@@ -61,8 +72,11 @@ Now the file has an extra row between each row of data values
 
 ## Write new data file of cleaned data
 
-    output.clean.file <- "./data/chalgrove_lake_4April2019.csv" 
-    # file name format = ./data/lake_date.csv
-    
     write.table(YSI, file = output.clean.file, row.names = F, quote = F, sep = ",")
+
+## Generate Metadata
+    
+    metadata <- c(YSI.metadata, "Deploy Begin:", as.character(deploy.begin), "Deploy End:", as.character(deploy.end))
+    writeLines(metadata, con = metadata.file) 
+    
     
---
+++ metadata/chalgrove_lake_general_metadata.md
@@ -0,0 +1,51 @@
+# General Metadata for Chalgrove Lake YSI Data 
+
+## Files
+
+Specific metadata for each deployment can be found as text files with the file format of:
+
+    chalgrove_lake_DDMonthYYYY_metadata.txt
+    
+Where DDMonthYYY is the date that the deployment ended.
+
+## File Created
+
+  * 2019-04-12 by KF
+  
+## File Modified
+
+## Description
+
+These data are from a YSI EXO2 multiparameter sonde deployed at approximatly 0.7 m in Chalgrove Lake (37.242983 N, -78.464116 W) in approximately 4 m of water.
+
+## Sonde Specifics
+
+  The specific sensors and SNs deployed are on the metadata specific to each deployment but generally the sonde has:
+  
+  * Temperature
+  * Conductivity
+  * pH
+  * ODO
+  * Total Algae
+  
+## Measurement Parameters, units, and Variable Names
+
+  * date.time = the combined date and time of the measurement (YYYY-MM-DD HH:MM:SS)
+  * date = the sampling date reported by the YSI (M/D/YYYY)
+  * time = the sampling time reported by the YSI (HH:MM:SS)
+  * fract_sec = the smapling fractions of a second reported by the YSI
+  * site_name = the site name on the YSI deployment (left blank)
+  * cond = the conductivity (uS/cm)
+  * nLF_cond = the nLF conductivity (uS/cm)
+  * perc_ODO = the percent oxygen saturation
+  * perc_local_ODO = the percent local oxygen saturation
+  * ODO_conc = the dissolved oxygen concentration (mg/L)
+  * sal = the salinity (psu)
+  * sp_cond = the specific conductivity (uS/cm)
+  * TDS = the total dissolved solids (mg/L)
+  * pH = the pH
+  * pH mV = the millivolts of the pH sensor
+  * temp = the temperature (dC)
+  * battery = the battery voltage
+  * cable_pwr = the cable power
+  
